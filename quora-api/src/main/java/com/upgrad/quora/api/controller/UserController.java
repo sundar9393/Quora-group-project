14b6d6e6062f917fa8fef75422f25a93d86b1bea
sundar93 <sundar9393@gmail.com>
2019-10-29 09:48:08 +0530

    Adding UserEntity and updations to signup method of user controller

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.POST, path ="/user/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> signup(SignupUserRequest signupUserRequest) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setAboutMe(signupUserRequest.getAboutMe());
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setMobile(signupUserRequest.getAboutMe());
+        userEntity.setUserName(signupUserRequest.getUserName());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        return null;
+    }
 }
--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -1,4 +1,174 @@
 package com.upgrad.quora.service.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@NamedQueries({
+
+})
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "firstname")
+    @NotNull
+    private String firstName;
+
+    @Column(name = "lastname")
+    @NotNull
+    private String lastName;
+
+    @Column(name = "username")
+    @NotNull
+    private String userName;
+
+    @Column(name = "email")
+    @NotNull
+    private String email;
+
+    @Column(name = "password")
+    @NotNull
+    private String password;
+
+    @Column(name = "salt")
+    @NotNull
+    private String salt;
+
+    @Column(name = "country")
+    @NotNull
+    private String country;
+
+    @Column(name = "aboutme")
+    private String aboutMe;
+
+    @Column(name = "dob")
+    @NotNull
+    private String dob;
+
+    @Column(name = "role")
+    @NotNull
+    private String role;
+
+    @Column(name = "contactnumber")
+    @NotNull
+    private String mobile;
+
+    public UserEntity () {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
