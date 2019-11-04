3950c2146564c18a3bce306d939e1d7e7fc6d587
sundar93 <sundar9393@gmail.com>
2019-11-05 00:32:31 +0530

    delete question api implementation

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -44,15 +44,33 @@ public class QuestionService {
     @Transactional(propagation = Propagation.REQUIRED)
     public QuestionEntity editQuestionContent(final String uuid, final String accessToken, final  String content) throws AuthorizationFailedException, InvalidQuestionException {
         UserAuthTokenEntity userAuthToken = authenticationService.signInValidation(accessToken);
-        QuestionEntity existingQuestion = questionDao.getQuestionByUuid(uuid);
-        if(existingQuestion == null) {
-            throw new InvalidQuestionException("QUES-001","Entered question uuid does not exist");
-        }
+        QuestionEntity existingQuestion = isQuestionPresent(uuid);
         if(existingQuestion.getUser().getId() != userAuthToken.getUser().getId()) {
             throw new AuthorizationFailedException("ATHR-003", "Only the question owner can edit the question");
         }
         existingQuestion.setContent(content);
         return questionDao.editQuestionContent(existingQuestion);
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity deleteQuestion(final String uuid, final String accessToken) throws AuthorizationFailedException, InvalidQuestionException {
+        UserAuthTokenEntity userAuthToken = authenticationService.signInValidation(accessToken);
+        QuestionEntity existingQuestion = isQuestionPresent(uuid);
+        if(existingQuestion.getUser().getId() != userAuthToken.getUser().getId()) {
+            throw new AuthorizationFailedException("ATHR-003","Only the question owner or admin can delete the question");
+        }
+        return questionDao.deleteQuestion(existingQuestion);
+    }
+
+
+    //Writing the logic in a method to avoid boiler plate code
+    private QuestionEntity isQuestionPresent(final String uuid) throws InvalidQuestionException {
+        QuestionEntity existingQuestion = questionDao.getQuestionByUuid(uuid);
+        if(existingQuestion == null) {
+            throw new InvalidQuestionException("QUES-001","Entered question uuid does not exist");
+        }
+        return existingQuestion;
     }
 
 
