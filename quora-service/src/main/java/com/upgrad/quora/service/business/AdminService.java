a81ceea6fffcc36c265a8fe66f1ae5a1fdfea3db
sundar93 <sundar9393@gmail.com>
2019-11-09 13:18:58 +0530

    unit test fixes

--- quora-service/src/main/java/com/upgrad/quora/service/business/AdminService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AdminService.java
@@ -6,9 +6,11 @@ import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class AdminService {
     @Autowired
     private UserDao userDao;
